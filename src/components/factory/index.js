c60e283ee90feb002fbae3b2ba6c1c90b016371d
王志勇 <wangzhiyong@szlanyou.com>
2023-10-25 10:27:24 +0800

    bug

---
+++ src/components/factory/index.js
@@ -0,0 +1,45 @@
+import FormFactory from './Form'
+import Factory from './Factory'
+import objectFunc from '@/utils/objectFunc'
+/**
+ * /**
+ * 创建组件属性
+ *@param {*} name
+ * @param {*} tag
+ * @param {*} label
+ * @param {*} groupName
+ * @param {*} icon
+ * @param {*} version
+ * @param {*} attrs
+ */
+export function create(name, tag, label, groupName, icon = '', version = '1.0.0', attrs) {
+  let newComponent
+  switch (groupName) {
+    case 'form':
+    // 表单类型的组件
+      newComponent = new FormFactory(name, tag, label, groupName, icon = '', version = '1.0.0', attrs)
+      break
+    default:
+      newComponent = new Factory(name, tag, label, groupName, icon = '', version = '1.0.0', attrs)
+      break
+  }
+}
+
+/**
+ * 复制组件 todo
+ * @param {*} configClone
+ */
+export function clone(configClone) {
+  return objectFunc(configClone)
+}
+
+/**
+ * 删除组件 todo
+ * @param {*} conTree
+ * @param {*} index
+ * @returns
+ */
+export function deleteChild(conTree, index) {
+  conTree.splice(index, 1)
+  return conTree
+}
